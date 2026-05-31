c2e60f017a0335cb42407b7a645939e598cd9ff1
Felipe Fernandes Alves <felipefernanalves@gmail.com>
2026-05-31 19:35:39 -0300

    interlude template

---
+++ scripts/interlude_template.gd
@@ -0,0 +1,8 @@
+extends Node2D
+@onready var dialog_scene: Control = $DialogScene
+@export var dialog_sequence: Array[DialogContent]
+
+signal scene_finished()
+
+func _on_dialog_scene_dialog_scene_finished() -> void:
+	scene_finished.emit()
