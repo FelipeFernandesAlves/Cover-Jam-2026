d0a85a3bae33e6bb3e1e1fa00fd38ea413b14364
Felipe Fernandes Alves <felipefernanalves@gmail.com>
2026-08-21 13:22:45 -0300

    feat: added dialogue_manager

---
+++ addons/shysplashscreen/scripts/splash_screen.gd
@@ -0,0 +1,27 @@
+class_name SplashScreen extends Control
+
+@export var next_scene: PackedScene
+@export var animation_player: AnimationPlayer
+@export var texture_rect: TextureRect
+
+var time: float
+var amp: float = 5
+var spd: float = 1.8
+
+signal splash_screen_ended()
+
+func _ready() -> void:
+	animation_player.play("fade")
+	splash_screen_ended.connect(func():
+		get_tree().change_scene_to_packed(next_scene)
+		)
+
+func _process(delta: float) -> void:
+	time += delta * spd
+	texture_rect.position.y = sin(time) * amp
+	if (Input.is_action_just_pressed("ui_accept")):
+		animation_player.stop()
+		on_end()
+
+func on_end():
+	splash_screen_ended.emit()
